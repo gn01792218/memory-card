8963330b70172bef979deb43eb8f03ed68b22760
DESKTOP-RPU80GU\gn01792218 <gn01792218@hotmail.com>
2022-03-18 12:43:32 +0800

    更新路由模式

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,8 +10,8 @@ const routes: Array<RouteRecordRaw> = [
 ]
 
 const router = createRouter({
-  // history: createWebHistory(process.env.BASE_URL),
-  history:createWebHashHistory(process.env.BASE_URL),
+  history: createWebHistory(process.env.BASE_URL),
+  // history:createWebHashHistory(process.env.BASE_URL),
   routes
 })
 export default router
