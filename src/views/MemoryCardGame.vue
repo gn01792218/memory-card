27fedabc71eacf4246a44e83db5cdfa81c79b21f
DESKTOP-RPU80GU\gn01792218 <gn01792218@hotmail.com>
2022-03-22 17:17:26 +0800

    測試圖片打包機制1-使用import引入靜態圖片資源

--- src/views/MemoryCardGame.vue
+++ src/views/MemoryCardGame.vue
@@ -9,11 +9,12 @@
 import { reactive } from "vue";
 import { memoryCard } from "@/types/global";
 import MemoryCardList from "@/components/MemoryCardGame/MemoryCardList.vue";
-
+import upImgUrl from '@/assets/images/memoryCard/memoryCard-back.webp';
+import downImgUrl from '@/assets/images/memoryCard/memoryCard-front.webp';
 const memoryCardList = reactive<memoryCard[]>([
   {
-    upImgPath: "/images/memoryCard/memoryCard-back.webp",
-    downImgPath: "/images/memoryCard/memoryCard-front.webp",
+    upImgPath: upImgUrl,
+    downImgPath: downImgUrl,
   },
   {
     upImgPath: "/images/memoryCard/memoryCard-back.webp",
