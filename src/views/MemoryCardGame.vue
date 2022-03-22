49a86f43cb6fbab9ea2b20c869ba57830b63a204
DESKTOP-RPU80GU\gn01792218 <gn01792218@hotmail.com>
2022-03-22 16:54:06 +0800

    更改圖片資源的資料夾位置

--- src/views/MemoryCardGame.vue
+++ src/views/MemoryCardGame.vue
@@ -12,12 +12,12 @@ import MemoryCardList from "@/components/MemoryCardGame/MemoryCardList.vue";
 
 const memoryCardList = reactive<memoryCard[]>([
   {
-    upImgPath: "src/assets/images/memoryCard/memoryCard-back.webp",
-    downImgPath: "src/assets/images/memoryCard/memoryCard-front.webp",
+    upImgPath: "/images/memoryCard/memoryCard-back.webp",
+    downImgPath: "/images/memoryCard/memoryCard-front.webp",
   },
   {
-    upImgPath: "src/assets/images/memoryCard/memoryCard-back.webp",
-    downImgPath: "src/assets/images/memoryCard/memoryCard-front.webp",
+    upImgPath: "/images/memoryCard/memoryCard-back.webp",
+    downImgPath: "/images/memoryCard/memoryCard-front.webp",
   },
 ]);
 </script>
