60be85c43551f3b8044f6d7da211282f112b64c0
DESKTOP-RPU80GU\gn01792218 <gn01792218@hotmail.com>
2022-03-24 11:32:04 +0800

    新增user Store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { createStore } from "vuex";
 import memoryCard from "./memoryCard";
+import user from './user'
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
   modules: {
     memoryCard,
+    user,
   },
 });
