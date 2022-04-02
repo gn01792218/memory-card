79c952fcfe47f97d1c562ba49abbea8c8f1930ba
MSI\gn01792218 <gn01792218@hotmail.com>
2022-04-02 15:08:41 +0800

    上版本:新增關卡計時機制

---
+++ docs/assets/useGame.0ae6fe87.js
@@ -0,0 +1,1 @@
+import{l as c,n as t}from"./vendor.f01bdc13.js";function L(){const e=c().state.game,r=t(()=>e.gameType),s=t(()=>e.gameTheme),n=t(()=>e.gameTypeList),m=t(()=>e.gameTypeList[r.value]),o=t(()=>e.currentLevel),a=t(()=>e.levelList),u=t(()=>e.memoryCardListObj);return{gameType:r,gameTheme:s,gameTypeList:n,gameThemeList:m,currentLevel:o,levelList:a,memoryCardListObj:u}}export{L as u};
