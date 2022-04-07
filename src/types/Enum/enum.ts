fce761ebe658cb6ddc8cbfbe5328fd666b3328a6
DESKTOP-RPU80GU\gn01792218 <gn01792218@hotmail.com>
2022-04-07 13:59:58 +0800

    新增記憶卡牌寶可夢和鬼滅之刃的enum

--- src/types/Enum/enum.ts
+++ src/types/Enum/enum.ts
@@ -1,28 +1,47 @@
-export enum gameTypeEnum{
-    other,
-    memoryCard,
-    mathGame,
+export enum gameTypeEnum {
+  other,
+  memoryCard,
+  mathGame
 }
-export enum gameThemeEnum{
-    pokemon,
-    ghostSlayer,
+export enum gameThemeEnum {
+  pokemon,
+  ghostSlayer
 }
-export enum pokemon{
-    皮卡丘,
-    妙蛙種子,
-    噴火龍,
-    傑尼龜,
-    六尾,
-    貓老大,
-    可達鴨,
+export enum pokemon {
+  皮卡丘,
+  妙蛙種子,
+  噴火龍,
+  傑尼龜,
+  六尾,
+  貓老大,
+  可達鴨,
+  西獅海壬,
+  果然翁,
+  小火馬,
+  好壞星,
+  月亮依布,
+  風速狗,
+  炙焰咆嘯虎,
+  閃焰王牌,
+  提布利姆,
+  索爾加雷歐
 }
-export enum ghostSlayer{
-    竈門炭治郎,
-    竈門禰豆子,
-    我妻善逸,
-    嘴平伊之助,
-    富岡義勇,
-    胡蝶忍,
-    煉獄杏壽郎,
-    鬼舞辻無慘,
+export enum ghostSlayer {
+  竈門炭治郎,
+  竈門禰豆子,
+  我妻善逸,
+  嘴平伊之助,
+  富岡義勇,
+  胡蝶忍,
+  煉獄杏壽郎,
+  鬼舞辻無慘,
+  不死川實彌,
+  栗花落香奈乎,
+  靖菟,
+  下弦之伍,
+  時透無一郎,
+  伊黑小芭內,
+  甘露寺蜜璃,
+  悲鳴嶼行冥,
+  宇髄天元,
 }
