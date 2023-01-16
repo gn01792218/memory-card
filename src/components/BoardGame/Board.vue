2cb97d92840aef26646c30028001ebe78cda67ed
jacky <jacky@metallurgietech.com>
2023-01-16 10:38:21 +0800

    新增棋盤、放置皇后

--- src/components/BoardGame/Board.vue
+++ src/components/BoardGame/Board.vue
@@ -3,23 +3,47 @@
         <section class="flex"
         v-for="row in size" :key="row"
         >
-            <div class="board-col"
+            <div class="board-col" :id="`board-${row}${col}`"
             v-for="col in size" :key="col"
             @click="clickBoardCol(row,col)"
+            @mouseover="hint(row,col)"
+            @mouseleave="removeHint(row,col)"
             >
-
             </div>
         </section>
     </section>
 </template>
 <script setup lang="ts">
+import useUtil from '@/composables/util/useUtil';
 interface BoardObj{
-    size:number
+    size:number,
+    openHint:boolean
 }
-defineProps<BoardObj>()
-
+const props = defineProps<BoardObj>()
+const { getAssetsFileURL } = useUtil()
+function getQueenImg(){
+    const queenImg = document.createElement('IMG') as HTMLImageElement
+    queenImg.src = getAssetsFileURL('images/boardGame/queen.png')
+    queenImg.classList.add('w-full')
+    queenImg.classList.add('h-full')
+    queenImg.classList.add('queen')
+    return queenImg
+}
 function clickBoardCol(row:number,col:number){
-    
+    //擺放皇后
+    const colElement = document.getElementById(`board-${row}${col}`)
+    //沒有圖片的話+圖片
+    const queenImg = colElement?.firstChild
+    if(queenImg) queenImg.remove()
+    else colElement?.appendChild(getQueenImg())
+}
+function hint(row:number,col:number){ //當游標移動到該col上時
+    if(!props.openHint) return 
+    //1.利用獲得之座標，判斷要顯示的col有哪些
+
+}
+function removeHint(row:number,col:number){ //當游標離開該格時，移除提示
+    if(!props.openHint) return 
 }
 
 </script>
--- src/views/BoardGameEightQueen.vue
+++ src/views/BoardGameEightQueen.vue
@@ -5,15 +5,43 @@
                 遊戲方式 : 請將所有皇后放進棋盤中，並且每個皇后都不能互相攻擊
             </section>
         </header>
-        <article>
-            <Board :size="boardSize"/>
-        </article>
+        <section class="mb-5">
+            <div>
+                <label for="">選擇棋盤Size</label>
+                <select class="text-black" v-model="boardSelectData.borderSize">
+                    <option
+                        v-for="option in boardSelectData.options" :key="option.value"
+                        :value="option.value"
+                    >{{ option.text }}</option>
+                </select>
+            </div>
+            <div>
+                是否開啟提示?
+                <input type="radio" id="openHint" :value="false" v-model="openHint">
+                <label for="openHint">是</label>
+                <input type="radio" id="openHint" :value="true" v-model="openHint">
+                <label for="openHint">否</label>
+            </div>
+        </section>
+        <Board :size="boardSelectData.borderSize" :open-hint="openHint"/>
     </main>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue'
 import Board from '@/components/BoardGame/Board.vue'
 const boardMinSize = 4
 const boardMaxSize = 8
-const boardSize = 4
+const boardSelectData = ref({
+    borderSize:4, //選中的值
+    options:[
+        { text:'4 X 4', value:4 },
+        { text:'5 X 5', value:5 },
+        { text:'6 X 6', value:6 },
+        { text:'7 X 7', value:7 },
+        { text:'8 X 8', value:8 },
+    ]
+})
+const openHint = ref(true)
+
 
 </script>
