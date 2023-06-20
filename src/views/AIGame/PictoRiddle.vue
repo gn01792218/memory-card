c9fc19425a6eb2d8ab1a894ec2e195cd42ec6c72
MSI\gn01792218 <gn01792218@hotmail.com>
2023-06-20 17:01:46 +0800

    remove openai from client side because it will only use in server side for sucuerity reason

--- src/views/AIGame/PictoRiddle.vue
+++ src/views/AIGame/PictoRiddle.vue
@@ -1,20 +1,25 @@
 <template>
     <section class="text-white">
-        <button class="rounded-full px-2 py-1 border-2 border-slate-300 hover:text-slate-300">產生謎題</button>
-        <img :src="riddleImageUrl" :alt="riddle">
-        <p>{{ riddle }}</p>
+        <button class="rounded-full px-2 py-1 border-2 border-slate-300 hover:text-slate-300"
+        @click="setRiddleImage"
+        >產生謎題</button>
+        <div>
+            <img :src="riddleImageUrl" :alt="riddle">
+        </div>
+        <p>{{ riddleHint }}</p>
     </section>
     
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
-import { Configuration, OpenAIApi  } from 'openai'
-const config = new Configuration({
-    apiKey:import.meta.env.VITE_KIDSGAME_OPENAI_KEY as string
-})
-console.log(import.meta.env.VITE_KIDSGAME_OPENAI_KEY)
-const riddle = ref('猜動物')
+const answer = ref('畫蛇添足')
+const riddleImagePrompt = ref(`幫我畫一個塗鴉風格的畫畫猜猜謎題塗鴉，謎底為'${answer.value}'`)
+const riddleHint = ref('猜動物')
 const riddleImageUrl = ref('')
+
+async function setRiddleImage (){
+    
+}
 
 </script>
